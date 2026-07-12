2db44a0b4a043a97dad4b4af15dde15336ecde4a
Jupalle Venkat <jupallivenkat634@gmail.com>
2026-07-13 00:43:08 +0530

    Fix Gmail SMTP configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -8,12 +8,16 @@ const sendEmail = async ({ name, email, subject, message }) => {
     });
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
+});
+    await transporter.verify();
+console.log("✅ Gmail SMTP Connected");
 
     // ===========================================
     // EMAIL TO YOU
